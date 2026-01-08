ae5a25a0c7787bab232fe485d086693c30688234
kapilkariya <kapilkariya1105@gmail.com>
2026-01-08 15:47:19 +0530

    drg

---
+++ Backend/config/passport.js
@@ -0,0 +1,13 @@
+var GoogleStrategy = require('passport-google-oauth20').Strategy;
+
+passport.use(new GoogleStrategy({
+    clientID: GOOGLE_CLIENT_ID,
+    clientSecret: GOOGLE_CLIENT_SECRET,
+    callbackURL: `${process.env.BACKEND_URL}/auth/google/callback`
+  },
+  function(accessToken, refreshToken, profile, cb) {
+    User.findOrCreate({ googleId: profile.id }, function (err, user) {
+      return cb(err, user);
+    });
+  }
+));
