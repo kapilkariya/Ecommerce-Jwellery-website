cf385511b866906deb2f7f5e84c67aa1b14e7d9c
kapilkariya <kapilkariya1105@gmail.com>
2026-01-18 09:30:11 +0530

    frontend

--- Backend/server.js
+++ Backend/server.js
@@ -42,5 +42,5 @@ app.get('/', (req, res) => {
 })
 
 
-app.listen(3000, () => console.log('Server started on PORT : 3000'))
-// export default app;  
+// app.listen(3000, () => console.log('Server started on PORT : 3000'))
+export default app;  
